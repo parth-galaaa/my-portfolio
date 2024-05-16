eb5e9c9384dc3b0ad5a2c459b95b29d9eb65788d
Parth Gala <parth1868134@gmail.com>
2024-05-16 09:18:48 -0400

    Added StarBackground

---
+++ src/app/components/StarBackground.jsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React, { useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial } from "@react-three/drei";
+
+import * as random from "maath/random/dist/maath-random.esm";
+
+const StarBackground = (props) => {
+  const ref = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 10;
+    ref.current.rotation.y -= delta / 15;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="$fff"
+          size={0.002}
+          sizeAttenuation={true}
+          dethWrite={false}
+        />
+      </Points>
+    </group>
+  );
+};
+
+const StarsCanvas = () => (
+  <div className="w-full h-screen fixed inset-0 z-[20] pointer-events-none">
+    <Canvas style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none' }} camera={{ position: [0, 0, 1] }}>
+      <StarBackground />
+    </Canvas>
+  </div>
+);
+
+export default StarsCanvas;
