0c23ff929d09e77ae74b8e0315874b996ac9c31f
Parth Gala <122494475+parth-galaaa@users.noreply.github.com>
2024-12-21 11:56:50 -0500

    speed and analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "./components/StarBackground";
-import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,14 @@ import ProjectSection from "./components/ProjectSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import ExperienceSection from "./components/ExperienceSection";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Analytics />
+      <SpeedInsights />
       <Navbar />
       <div class="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
