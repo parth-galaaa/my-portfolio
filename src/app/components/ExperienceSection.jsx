d96dd5a1224fe30382ea2dceb971eada7cd0dd35
Parth Gala <122494475+parth-galaaa@users.noreply.github.com>
2025-01-13 15:48:05 -0500

    changes to ExperienceSection

--- src/app/components/ExperienceSection.jsx
+++ src/app/components/ExperienceSection.jsx
@@ -6,32 +6,32 @@ const ExperienceSection = () => {
             id: 1,
             companyName: "Ground Effects Ltd",
             position: "Software Developer Co-op",
-            duties: ["Integrated with the PHP code base to develop solutions addressing diverse task requirements",
-                "Proficiently used SQL to extract and manipulate data using complex queries and stored procedures, supporting seamless database operations and facilitated informed decision-making",
-                "Developed web applications and interactive pages, using Next.js to display real time data retrieved from database tables",
-                "Analyzed data using Power BI and Report Builder to generate comprehensive reports that inform decision-making",
-                "Designed and deployed cloud-based workflows to automate routine tasks, improving operational efficiency and reducing manual effort by 60%"],
+            duties: ["Designed and implemented optimized PHP code to address diverse project requirements, including database integration, back-end processing, and user interface functionality",
+                "Leveraged complex SQL queries and stored procedures to ensure seamless data extraction and database operations",
+                "Developed web applications and interactive pages, using Next.js to display real time data retrieved from database",
+                "Analyzed data with visualization tools to produce actionable reports, driving data-driven decisions",
+                "Deployed cloud-based workflows to automate routine tasks, improving operational efficiency and reducing manual effort by almost 80%"],
             duration: "Sept 2023 - Aug 2024",
         },
         {
             id: 2,
             companyName: "Malad Gas Service, BPCL",
-            position: "Data Analyst Intern",
+            position: "Data Analyst",
             duties: [
-                "Analyzed and processed data using SQL to create and test new functionalities along with integrating databases to generate reports to track monthly statistics",
-                "Designed data visualizations to effectively communicate data insights by engaging with interactive dashboards",
-                "Collaborated with a team of 4-5 members to drive sales, and provide high-quality customer-service enhancing team performance and productivity"
+                "Optimizing monthly statistics reporting by analyzing large datasets and integrating databases using SQL, improving accuracy by 40% and delivering valuable insights",
+                "Designing interactive visualizations that simplified data insights, boosting stakeholder engagement and improving strategic business outcomes",
+                "Collaborating with a team of 4-5 members to drive sales, and provide high-quality customer-service enhancing team performance and productivity"
             ],
-            duration: "Jun 2022 - Jun 2024",
+            duration: "Jun 2023 - Present",
         },
         {
             id: 3,
             companyName: "University of Windsor",
             position: "Teaching Assistant",
             duties: [
-                "Planned and facilitated sessions for 20+ students on topics in Computer Science and provided personalized feedback",
+                "Planned and facilitated labs for 30+ students on topics in Computer Science and provided personalized feedback",
                 "Liaised with professor regarding development opportunities, challenges and assisting with conducting labs for students",
-                "Coordinated with grading and assessment of 15+ student work, including homework, quizzes, and tests"
+                "Coordinated with grading and assessment of 25+ student work, including assignments, quizzes, and tests"
             ],
             duration: "Jan 2023 - Dec 2024",
         }
